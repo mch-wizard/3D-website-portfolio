15ad4660cb5436694df233bda1dde93575957ec3
Marcin <mch.wizard@gmail.com>
2023-05-06 22:25:25 +0200

    Hero and canvas components started, problem with @react-three to solve

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas>
+      
+    </Canvas>
+  )
+}
+
+export default Computers;
